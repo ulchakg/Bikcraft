c06b4e77ac45262865cc27c6cdb6d537caacdfb4
Guilherme Ulchak <guilherme.ulchak@gmail.com>
2021-06-01 20:26:33 -0300

    adicionando as animações de slide na pag portfólio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+if(window.SimpleSlide) {
+new SimpleSlide({
+  slide: 'quote', // nome do atributo data-slide="principal"
+  nav: false, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
+
+new SimpleSlide({
+  slide: 'products_img',
+  nav: true,
+  auto:true,
+  time: 5000,
+  pauseOnHover: false,
+});
+}
